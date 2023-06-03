9a0ec783312691a22ccbf196988a002852cfb8d4
LT_Name <572413378@qq.com>
2023-06-03 22:19:29 +0800

    修复命令监听空指针

--- src/main/java/cn/lanink/hotpotato/HotPotato.java
+++ src/main/java/cn/lanink/hotpotato/HotPotato.java
@@ -33,7 +33,7 @@ import java.util.*;
  */
 public class HotPotato extends PluginBase {
 
-    public static final String VERSION = "1.2.1 git-fcc7b7c";
+    public static final String VERSION = "?";
     public static boolean debug = false;
     public static final Random RANDOM = new Random();
     private static HotPotato hotPotato;
